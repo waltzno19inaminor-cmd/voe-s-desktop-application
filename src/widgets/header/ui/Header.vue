2d8b19c7b663b912bcb8d8625ea8e684df22572a
Evan Vosh <evanvosh@MacBook-Air-Evan.local>
2026-02-18 20:19:10 +0100

    fix: Add error UI and broaden shell permissions

--- src/widgets/header/ui/Header.vue
+++ src/widgets/header/ui/Header.vue
@@ -88,6 +88,15 @@
          
         </div>
       </nav>
+      </nav>
+    </div>
+    <div v-if="auth.error" class="fixed top-24 left-1/2 -translate-x-1/2 z-50 bg-red-600 text-white px-6 py-3 rounded-full shadow-xl flex items-center gap-4 animate-bounce">
+      <span>{{ auth.error }}</span>
+      <button @click="auth.setError(null)" class="hover:bg-red-700 rounded-full p-1">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+        </svg>
+      </button>
     </div>
   </header>
 </template>
